34c48cb540e93c11396ffedb3f77e75255711358
Siobhan Duignan <siobhan.duignan@daqri.com>
2019-05-08 17:50:25 +0100

    Parallax with offset tex

--- Cheesed Burger Chase/Assets/Scripts/ParallaxLayerScroller.cs
+++ Cheesed Burger Chase/Assets/Scripts/ParallaxLayerScroller.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class ParallaxLayerScroller : MonoBehaviour
 {
     [SerializeField]
@@ -9,23 +10,31 @@ public class ParallaxLayerScroller : MonoBehaviour
     [SerializeField]
     private float followRate = 0.25f;
     private Vector3 prevPos;
+    private SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         prevPos = followee.transform.position;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        float spriteWidth = spriteRenderer.bounds.size.x;
+        Debug.Log("GameObject: " + gameObject.name + "\tWidth: " + spriteWidth.ToString());
+        spriteRenderer.material.SetTextureOffset("_MainTex", new Vector2(0.333f, 0));
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        float xOffset = followRate * (followee.position.x - prevPos.x);
+        transform.position = new Vector3(transform.position.x + xOffset, transform.position.y, transform.position.z);
+        prevPos = followee.transform.position;
 
     }
 
     private void LateUpdate()
     {
-        float xOffset = followRate * (followee.position.x - prevPos.x);
-        transform.position = new Vector3(transform.position.x + xOffset, transform.position.y, transform.position.z);
-        prevPos = followee.transform.position;
+
+
     }
 }
---
+++ Cheesed Burger Chase/Assets/Scripts/ParallaxMeshController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class ParallaxMeshController : MonoBehaviour
+{
+    [SerializeField]
+    private Texture backgroundTexture;
+    [SerializeField]
+    private Transform followee;
+    [SerializeField]
+    private float followRate = 0.25f;
+    private float prevPos;
+    private MeshRenderer meshRenderer;
+    private float quadWidth;
+    private float offset = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prevPos = followee.transform.position.x;
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material.SetTexture("_MainTex", backgroundTexture);
+        quadWidth = transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(followee.position.x, transform.position.y, transform.position.z);
+
+        float distTravelled = prevPos - followee.transform.position.x;
+        offset += (distTravelled / quadWidth) * (followRate - 1);
+
+        meshRenderer.material.mainTextureOffset = new Vector2(offset, 0);
+        prevPos = followee.transform.position.x;
+    }
+}
